76a9f4186c5eb693915948e9f6b4d9d76e346db0
orran <orran108@yahoo.co.uk>
2023-12-20 13:37:50 +0000

    changed design

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ let isModalOpen = false;
 let contrastToggle = false;
 const scaleFactor = 1 / 20;
 
-function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape");
-  const x = event.clientX * scaleFactor;
-  const y = event.clientY * scaleFactor;
+// function moveBackground(event) {
+//   const shapes = document.querySelectorAll(".shape");
+//   const x = event.clientX * scaleFactor;
+//   const y = event.clientY * scaleFactor;
 
-  for (let i = 0; i < shapes.length; ++i) {
-    const isOdd = i % 2 !== 0;
-    const booInt = isOdd ? -1 : 1;
-    shapes[i].style.transform = `translate(${x * booInt}px, ${y * booInt}px)`;
-  }
-}
+//   for (let i = 0; i < shapes.length; ++i) {
+//     const isOdd = i % 2 !== 0;
+//     const booInt = isOdd ? -1 : 1;
+//     shapes[i].style.transform = `translate(${x * booInt}px, ${y * booInt}px)`;
+//   }
+// }
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
